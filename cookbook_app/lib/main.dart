cfaedc9d44c941e4383852898e7c121de71cbca1
Kaylee <KayleeBigelow@gmail.com>
2021-02-28 16:56:46 -0500

    adding input fields for second page of create recipe

--- cookbook_app/lib/main.dart
+++ cookbook_app/lib/main.dart
@@ -41,12 +41,114 @@ class FinishRecipeRoute extends StatefulWidget {
 }
 
 class _FinishRecipeState extends State<FinishRecipeRoute> {
+  bool checkboxCalories = false;
+  bool checkboxPublic = false;
+  String dropdownTags = 'None';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Create a Recipe"),
       ),
+      body: Center(
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(width: 0.0, height: 0.0),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          "Add Tags",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 4,
+                          child: DropdownButton<String>(
+                            value: dropdownTags,
+                            icon: Icon(Icons.arrow_drop_down),
+                            iconSize: 50,
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                            onChanged: (String newValue) {
+                              setState(() {
+                                dropdownTags = newValue;
+                              });
+                            },
+                            items: <String>['None', 'Breakfast', 'Dinner']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ))
+                    ]),
+                    Padding(padding: EdgeInsets.only(bottom: 30)),
+                    Row(children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(width: 0.0, height: 0.0),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          "Add Calories",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 4,
+                          child: Transform.scale(
+                              scale: 1.4,
+                              child: Checkbox(
+                                value: checkboxCalories,
+                                onChanged: (value) {
+                                  setState(() {
+                                    checkboxCalories = !checkboxCalories;
+                                  });
+                                },
+                              )))
+                    ]),
+                    Padding(padding: EdgeInsets.only(bottom: 30)),
+                    Row(children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(width: 0.0, height: 0.0),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          "Make Public",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 4,
+                          child: Transform.scale(
+                              scale: 1.4,
+                              child: Checkbox(
+                                value: checkboxPublic,
+                                onChanged: (value) {
+                                  setState(() {
+                                    checkboxPublic = !checkboxPublic;
+                                  });
+                                },
+                              )))
+                    ]),
+                  ]))),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.arrow_back), label: "Back"),
@@ -193,8 +295,7 @@ class _CreateRecipeState extends State<CreateRecipeRoute> {
                       keyboardType: TextInputType.multiline,
                       maxLines: 8,
                       decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          hintText: "..."),
+                          border: const OutlineInputBorder(), hintText: "..."),
                     ),
                   )
                 ]),
@@ -211,11 +312,10 @@ class _CreateRecipeState extends State<CreateRecipeRoute> {
                 Row(children: <Widget>[
                   Expanded(
                       child: TextField(
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 8,
-                        decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            hintText: "..."),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(), hintText: "..."),
                   )),
                 ])
               ]),
