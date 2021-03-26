30c50f34cb7069bed359fe5e6850d1b8b0cd4260
damianMietus <damianmietus1@gmail.com>
2021-03-26 15:07:02 -0400

    Update main.dart

--- cookbook_app/lib/main.dart
+++ cookbook_app/lib/main.dart
@@ -1,169 +1,220 @@
 import 'dart:developer';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 
-class RecipeStorage {
-  static Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
+import 'package:flutter/material.dart';
+import './createRecipeScreen.dart' as createRecipeScreen;
+import './updateFoldersScreen.dart' as updateFoldersScreen;
+import './RecipeStorage.dart' as RSClass;
+import './searchRecipeScreen.dart' as searchRecipeScreen;
+import './viewFolderScreen.dart' as viewFolderScreen;
+import './viewExistingRecipe.dart' as viewExistingRecipeScreen;
+
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Yum Binder', storage: RSClass.RecipeStorage()),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final RSClass.RecipeStorage storage;
+  final String deleteFilePath;
+
+  MyHomePage({Key key, this.title,  this.deleteFilePath, @required this.storage}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List allFiles;
+  List allFolders;
+  List topFiles;
+  String view;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: widget.storage.getAllFiles(),
+        builder: (context, AsyncSnapshot<List> values) {
+          if (values.hasData) {
+            var folders = RSClass.RecipeStorage.getFolders(values.data);
+            var topFilesLocal =
+                RSClass.RecipeStorage.getTopLevelRecipes(values.data);
+            allFiles = values.data;
+            allFolders = folders;
+            topFiles = topFilesLocal;
+            view = 'top';
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title),
+              ),
+              body: Column( mainAxisAlignment: MainAxisAlignment.center, children: [
+                    SizedBox(height:250,child: ListView(children: getListItemsFolders())),
+                    Expanded(child:ListView(children: getListItems())),
+                  ]),
+
+              drawer: Drawer(
+                // Add a ListView to the drawer. This ensures the user can scroll
+                // through the options in the drawer if there isn't enough vertical
+                // space to fit everything.
+                child: ListView(
+                  // Important: Remove any padding from the ListView.
+                  padding: EdgeInsets.zero,
+                  //child: Column(
+                  children: <Widget>[
+                    DrawerHeader(
+                      child: Text('Yum Binder'),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('Public Recipes'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  searchRecipeScreen.SearchRecipeRoute()),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Share a Recipe'),
+                      onTap: () {
+                        // Update the state of the app.
+                        // ...
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Update Folders'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  updateFoldersScreen.UpdateFoldersRoute(folders: allFolders, allFiles: allFiles)),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Create a Recipe'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  createRecipeScreen.CreateRecipeRoute()),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Logout'),
+                      onTap: () {
+                        // Update the state of the app.
+                        // ...
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            createRecipeScreen.CreateRecipeRoute()),
+                  );
+                },
+                tooltip: 'Go to Create Recipe',
+                child: Icon(Icons.add),
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 
-  static Future<File> _localFile(String fileName) async {
-    final path = await _localPath;
-    return File('$path/$fileName');
-  }
-
-  static Future<File> writeRecipe(Map recipeInfo) async {
-    final file = await _localFile(recipeInfo['path']+'-'+recipeInfo['name']);
-    return file.writeAsString('$recipeInfo');
-  }
-
-  static Future<File> writeFolder(Map info) async {
-    final file = await _localFile('folder'+'-'+info['name']);
-    return file.writeAsString('$info');
-  }
-
-  static Future<int> deleteFile(String fileName) async{
-    try {
-      var temp = fileName.toString();
-      var temp2 = temp.split("/");
-      var curr = temp2[temp2.length-1];
-      final file = await _localFile('$curr');
-      var tempy = await file.delete();
-      return 1;
-    } catch (e) {
-      return 0;
+  List<ListTile> getListItems() {
+    if (view == "top") {
+      return (topFiles
+          .asMap()
+          .map((i, item) => MapEntry(i, buildListTile(item, i)))
+          .values
+          .toList());
+    } else {
+      print(allFiles);
+      print("ok");
+      List curr = RSClass.RecipeStorage.getFilesInFolder(allFiles, view);
+      curr
+          .asMap()
+          .map((i, item) => MapEntry(i, buildListTile(item, i)))
+          .values
+          .toList();
     }
   }
 
-  static Future<int> renameFolder(String fileName, String newFileName) async{
-    try {
-      var delete = await deleteFile(fileName);
-      var make = generateFolder(newFileName);
-      //TODO : rename all files in folder
-      return 1;
-    } catch (e) {
-      return 0;
-    }
-  }
-
-  Future<String> readRecipe(String filename) async {
-    print("entered: "+ filename);
-    try {
-      var temp = filename.toString();
-      var temp2 = temp.split("/");
-      var curr = temp2[temp2.length-1];
-      final file = await _localFile('$curr');
-      print("Awaited: "+ file.toString());
-      String contents = await file.readAsString();
-      //print()
-      return contents;
-    } catch (e) {
+  List<ListTile> getListItemsFolders() {
+    if (view == "top") {
+      return (allFolders
+          .asMap()
+          .map((i, item) => MapEntry(i, buildFolderTile(item, i)))
+          .values
+          .toList());
+    } else {
       return null;
     }
   }
 
-  Future<List> getAllFiles() async {
-    try {
-      final path = await _localPath;
-      List files = Directory('$path/').listSync();
-      return files;
-    } catch (e) {
-      return null;
-    }
+  ListTile buildFolderTile(String item, int index) {
+    return ListTile(
+      key: ValueKey(item),
+      title: Text(item),
+      leading: Icon(Icons.folder_outlined),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => viewFolderScreen.ViewFolderRoute(
+                  folder: item, allFiles: allFiles)),
+        );
+      },
+    );
   }
 
-  static List getFolders(List allFiles) {
-    try {
-      List files = [];
-      for(var i = 0; i < allFiles.length; i++) {
-        var temp = allFiles[i].toString();
-        var temp2 = temp.split("/");
-        var curr = temp2[temp2.length-1];
-        if(curr.startsWith('folder-')){
-          files.add(curr.split("-")[1].replaceAll("'", ""));
-        }
-      }
-      return files;
-    } catch (e) {
-      return null;
-    }
-  }
+  @override
+  ListTile buildListTile(String item, int index) {
 
-  static List getTopLevelRecipes(List allFiles) {
-    try {
-      List files = [];
-      for(var i = 0; i < allFiles.length; i++) {
-        var temp = allFiles[i].toString();
-        var temp2 = temp.split("/");
-        var curr = temp2[temp2.length-1];
-        if(curr.startsWith('null-')){
-          files.add(temp.replaceAll("'", "").replaceAll("File: ", ""));
-        }
-      }
-      return files;
-    } catch (e) {
-      return null;
-    }
-  }
+    return ListTile(
+      key: ValueKey(item),
+      title: Text(item.split("-")[1]),
+      leading: Icon(Icons.text_snippet),
 
-  static List getFilesInFolder(List allFiles, String folder) {
-    try {
-      List files = [];
-      for(var i = 0; i < allFiles.length; i++) {
-        var temp = allFiles[i].toString();
-        var temp2 = temp.split("/");
-        var curr = temp2[temp2.length-1];
-        if(curr.startsWith('$folder'+'-')){
-          files.add(curr);
-        }
-      }
-      return files;
-    } catch (e) {
-      return null;
-    }
+      onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => viewExistingRecipeScreen.ViewExistingRecipe(
+                    name: item, storage: RSClass.RecipeStorage())),
+          );
+      },
+    );
   }
-
-   static Map generateRecipe(
-      String name,
-      String ingredients,
-      String instructions,
-      bool calories,
-      String tags,
-      bool public,
-      String image,
-      String path) {
-    try {
-      var info = {
-        "type": "file",
-        "name": '$name',
-        "ingredients": '$ingredients',
-        "instructions": '$instructions',
-        "calories": '$calories',
-        "tags": '$tags',
-        "public": '$public',
-        "image": '$image',
-        "path": '$path'
-      };
-      writeRecipe(info);
-      return info;
-    } catch (e) {
-      return null;
-    }
-  }
-
-   static Map generateFolder(String name) {
-    var info = {"type": "folder",
-                "name": '$name',
-                "order": 1};
-    writeFolder(info);
-    return info;
-  }
-
-  static int deleteFolder(String name) {
-    var j = deleteFile(name);
-    print(j);
-    return 1;
-  }
-
 }
