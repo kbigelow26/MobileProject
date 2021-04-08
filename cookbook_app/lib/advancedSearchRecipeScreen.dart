1c40f608f8330d60fe81ec11ac66a82345348d97
Kaylee <KayleeBigelow@gmail.com>
2021-04-08 11:08:56 -0400

    changes to advanced search

--- cookbook_app/lib/advancedSearchRecipeScreen.dart
+++ cookbook_app/lib/advancedSearchRecipeScreen.dart
@@ -1,18 +1,26 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import './main.dart' as homeScreen;
-import './finishRecipeScreen.dart' as finishRecipeScreen;
 import './searchRecipeScreen.dart' as searchRecipeScreen;
 import 'package:textfield_tags/textfield_tags.dart';
 
 class SearchRecipeRoute extends StatefulWidget {
+  SearchRecipeRoute({Key key, this.allFolders}) : super(key: key);
+  final List allFolders;
+
   @override
   _SearchRecipeState createState() => _SearchRecipeState();
 }
 
 class _SearchRecipeState extends State<SearchRecipeRoute> {
+  final _formKey = GlobalKey<FormState>();
+  var nameController = TextEditingController();
+  var ingredController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    ingredController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,48 +30,58 @@ class _SearchRecipeState extends State<SearchRecipeRoute> {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(children: <Widget>[
-                    Expanded(
-                        flex: 5,
-                        child: TextField(
-                          keyboardType: TextInputType.multiline,
-                          textAlignVertical: TextAlignVertical.top,
-                          maxLines: null,
-                          decoration:
-                          InputDecoration(hintText: "Name"),
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(children: <Widget>[
+                      Expanded(
+                          flex: 5,
+                          child: TextFormField(
+                            controller: nameController,
+                            keyboardType: TextInputType.multiline,
+                            textAlignVertical: TextAlignVertical.top,
+                            maxLines: null,
+                            decoration:
+                            InputDecoration(hintText: "Name"),
+                            validator: (value){
+                              if (value.isEmpty) {
+                                return 'Please enter search criteria';
+                              } else {
+                                return null;
+                              }
+                            },
+                          )),
+                    ]),
+
+                    Padding(padding: EdgeInsets.only(bottom: 20)),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Ingredients",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                          ),
                         )),
-                  ]),
-
-                  Padding(padding: EdgeInsets.only(bottom: 20)),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Ingredients",
-                        style: TextStyle(
-                          fontSize: 18.0,
+                    Padding(padding: EdgeInsets.only(bottom: 10)),
+                    TextFieldTags(
+                        tagsStyler: TagsStyler(
+                            tagTextStyle: TextStyle(fontWeight: FontWeight.bold),
+                            tagDecoration: BoxDecoration(color: Colors.blue[300], borderRadius: BorderRadius.circular(8.0), ),
+                            tagCancelIcon: Icon(Icons.cancel, size: 18.0, color: Colors.blue[900]),
+                            tagPadding: const EdgeInsets.all(6.0)
                         ),
-                      )),
-                  Padding(padding: EdgeInsets.only(bottom: 10)),
-                  TextFieldTags(
-                      //tags: ['university', 'college', 'music', 'math'],
-                      tagsStyler: TagsStyler(
-                          tagTextStyle: TextStyle(fontWeight: FontWeight.bold),
-                          tagDecoration: BoxDecoration(color: Colors.blue[300], borderRadius: BorderRadius.circular(8.0), ),
-                          tagCancelIcon: Icon(Icons.cancel, size: 18.0, color: Colors.blue[900]),
-                          tagPadding: const EdgeInsets.all(6.0)
-                      ),
-                      textFieldStyler: TextFieldStyler(hintText: " ", helperText: " "),
-                      onTag: (tag) {},
-                      onDelete: (tag) {}
-                  ),
+                        textFieldStyler: TextFieldStyler(hintText: " ", helperText: " "),
+                        onTag: (tag) {},
+                        onDelete: (tag) {}
+                    ),
 
-                  Padding(padding: EdgeInsets.only(bottom: 20)),
-                ]),
+                    Padding(padding: EdgeInsets.only(bottom: 20)),
+                  ]),
+            ),
           ),
         ),
       ),
--- cookbook_app/lib/searchRecipeScreen.dart
+++ cookbook_app/lib/searchRecipeScreen.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import './main.dart' as homeScreen;
 
-//import './finishRecipeScreen.dart' as finishRecipeScreen;
 import './advancedSearchRecipeScreen.dart' as advancedSearchRecipeScreen;
 import './RecipeStorage.dart' as RSClass;
 import './viewExistingRecipe.dart' as viewExistingRecipeScreen;
